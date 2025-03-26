a6670e576dee44a4e4b0f0cd42141e24b81124d6
rana0518 <rukminirana6@gmail.com>
2025-03-26 22:54:11 +0530

    first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+import datetime
+
+# Print Hello, World!
+print("Hello, World!")
+
+# Print the current date and time
+now = datetime.datetime.now()
+print("Current Date and Time:", now.strftime("%Y-%m-%d %H:%M:%S"))
